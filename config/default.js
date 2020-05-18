3cfc19457072e9be276d4c43776616a587f3564c
Oladapo Omonayajo <o.omonayajo@gmail.com>
2020-05-18 08:22:23 +0800

    Added typeorm config

---
+++ config/default.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const env = process.env;
+
+module.exports = {
+  APP_NAME: env.APP_NAME || 'Remote Expert API',
+  PORT: +env.PORT || 8000,
+  NODE_ENV: env.NODE_ENV || 'development',
+  DB_DIALECT: env.DB_DIALECT || 'mongodb',
+  DB_HOST: env.DB_HOST || 'localhost',
+  DB_DATABASE: env.DB_DATABASE || 'remote_expert',
+  DB_USERNAME: env.DB_USERNAME || 'root',
+  DB_PASSWORD: env.DB_PASSWORD || 'rootpass',
+  DB_PORT: +env.DB_PORT || 27017,
+  APP_VERSION: '0.0.1',
+};
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { get } from 'config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: get('DB_DIALECT'),
+      host: get('DB_HOST'),
+      username: get('DB_USERNAME'),
+      password: get('DB_PASSWORD'),
+      synchronize: true,
+      entities: [],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
